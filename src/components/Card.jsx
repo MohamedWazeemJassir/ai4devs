de530e42026b3f2c1b2f5b0d3576189cc2193478
wazeem <mohamedwazeemjassir@gmail.com>
2026-06-29 22:56:25 +0530

    redirect to homepage when logo is clicked

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card(prop) {
   return (
-    <div className="ml-5 mb-5 border-2 p-6 size-fit rounded">
+    <div className="ml-5 mb-5 border p-6 size-fit rounded">
       <h1 className="text-3xl p-3">{prop.name}</h1>
       <button type="button" className="bg-gray-300 text-black pe-3 rounded ps-3 mr-3">
         <a href={prop.url} target="_blank">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex h-15 mb-18 pt-5 font-mono ">
-      <h1 className="pl-20 pt-5 pb-3 text-2xl">ai4devs</h1>
+      <a href="/"><h1 className="pl-20 pt-5 pb-3 text-2xl">ai4devs</h1></a>
       <nav className="float-end whitespace-pre-wrap pl-20 pt-5 pb-3 text-xl">
         <a className="pr-5" href="">
           About
