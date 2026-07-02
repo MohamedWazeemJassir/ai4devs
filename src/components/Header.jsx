8150cee6d59b7b5c4925839b667e5f1a08532080
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-02 22:17:53 +0530

    added newsletter form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ function Header() {
   return (
     <header className="flex h-15 mb-18 pt-5 font-mono ">
       <a href="/" className="text-2xl ml-15 mt-5">
-        ai4devs 
-        </a>
+        ai4devs
+      </a>
       <nav className="pl-20 pt-5 pb-3 text-xl">
-        <a className="mr-5" href="">
-          About
-        </a>
-        <a className="mr-5" href="">
-          Sign Up
-        </a>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="border rounded border-amber-50 p-2 h-8 mr-3 ml-200"
+          placeholder="john@expample.com"
+        />
+        <button type="submit" className="">Subscribe</button>
       </nav>
     </header>
   );
