d636aa981c6d3e9730bb6193d1ad3e4a282475c6
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-05 00:04:58 +0530

    add external link icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 function Card(prop) {
   return (
     <div className="ml-5 mb-5 border shadow-gray-300 shadow-md p-6 w-1/4 rounded delay-75 duration-150 transition hover:-translate-y-1 hover:bg-gray-500 space-y-3">
       <div id="top">
-        <a className=" flex justify-end" href={prop.url} rel="noopener noreferrer" target="_blank">
-          <button type="button" className="text-white rounded">
-            Visit
-          </button>
+        <a
+          className=" flex justify-end"
+          href={prop.url}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaExternalLinkAlt />
         </a>
       </div>
       <div id="middle">
