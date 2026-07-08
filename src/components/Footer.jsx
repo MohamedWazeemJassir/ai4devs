04e60d233d888fb62ac1cea99843ef32efc14fbf
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-08 20:55:07 +0530

    complete footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className="sticky bottom-0 left-0 w-full text-center font-mono">
-      © 2026 ai4devs · Made for developers who love to ship fast
-    </div>
+    <footer>
+      <hr className="text-gray-700 opacity-100 mt-5" />
+      <p className="py-8 px-4 w-full text-center font-mono text-gray-400">
+        © {currentYear} ai4devs · Made for developers who love to ship fast.
+      </p>
+    </footer>
   );
 }
 
