7615877fc43e2c6937e0995a420f1099f13d4503
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-12 23:57:51 +0530

    shift the external link icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,16 @@ function Card(prop) {
   const Avatar = Icons[prop.logo]?.Avatar;
   return (
     <div className="border overflow-hidden shadow-gray-300 hover:shadow-gray-100 p-6 w-1/4 rounded delay-75 duration-150 transition hover:-translate-y-1 hover:bg-gray-500 space-y-3">
-      <a
-        className="flex justify-end"
-        href={prop.url}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <FaExternalLinkAlt />
-      </a>
       <div id="middle" className="flex gap-3 items-center">
         {Avatar && <Avatar size={40} />}
         <h1 className="text-2xl">{prop.name}</h1>
+        <a
+          href={prop.url}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaExternalLinkAlt />
+        </a>
       </div>
       <p className="pl-2">{prop.description}</p>
       <div id="bottom" className="flex gap-2">
