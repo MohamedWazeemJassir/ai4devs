3971f08f0dd13981f4255a635d7e7cd39b073c9f
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-13 23:08:02 +0530

    change footer colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <hr className="text-gray-700 opacity-100 mt-5" />
-      <p className="py-8 px-4 w-full text-center font-mono text-gray-400">
+      <hr className="text-amber-50 opacity-100 mt-5" />
+      <p className="py-8 px-4 w-full text-center font-mono text-amber-50">
         © {currentYear} ai4devs · Made for developers who love to ship fast.
       </p>
     </footer>
