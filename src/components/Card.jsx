f66a310d96edc498f2a3d784a8865ca33963c742
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-14 22:37:56 +0530

    style and make cards mobile responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,21 @@
 import { FaExternalLinkAlt } from "react-icons/fa";
 import * as Icons from "@lobehub/icons";
 
-function Card(prop) {
-  const { logo, name, url, description, category, pricing } = prop;
+function Card({ logo, name, url, description, category, pricing }) {
   const Avatar = Icons[logo]?.Avatar;
   return (
-    <div className="border shadow-gray-300 hover:shadow-gray-100 p-6 w-1/4 rounded delay-75 duration-150 transition hover:-translate-y-1 hover:bg-gray-500 space-y-3">
-      <div id="middle" className="flex gap-3 items-center">
+    <div className="border bhover:shadow-gray-100order-zinc-800 bg-zinc-900 shadow-gray-300 hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500 p-6 w sm:w-1/5 rounded delay-75 duration-150 transition hover:-translate-y-2 space-y-3">
+      <div className="flex gap-3 items-center">
         {Avatar && <Avatar size={40} />}
-        <h1 className="text-2xl">{name}</h1>
+        <span className="text-2xl">{name}</span>
         <a href={url} rel="noopener noreferrer" target="_blank">
           <FaExternalLinkAlt />
         </a>
       </div>
       <p>{description}</p>
-      <div id="bottom" className="flex gap-2">
-        <p className="p-2 rounded bg-red-500">{category}</p>
-        <p className="p-2 rounded bg-yellow-600">{pricing}</p>
+      <div className="flex gap-2">
+        <span className="p-2 rounded bg-red-500">{category}</span>
+        <span className="p-2 rounded bg-yellow-600">{pricing}</span>
       </div>
     </div>
   );
