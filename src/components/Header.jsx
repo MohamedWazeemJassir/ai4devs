62632ab7833ffe3b068aec61f711f4c282afad15
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-17 23:57:20 +0530

    fix header elements alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 function Header() {
   return (
-    <header className="w-full py-12 font-mono text-center">
+    <header className="w-full flex py-12 font-mono text-center">
       <a
         href="/"
-        className="absolute top-10 left-20 text-2xl font-medium tracking-tight hover:text-violet-500"
+        className="top-10 ml-25 text-2xl font-medium tracking-tight hover:text-violet-500"
       >
         ai4devs
       </a>
 
-      <form action="" className="absolute top-10 right-20">
+      <form action="" className="w-1/3 top-10 mr-15 flex ml-auto">
         <input
           type="email"
           name="email"
           id="email"
-          className="rounded px-5 h-10 focus:border-sky-500 disabled:border-gray-200"
+          className="rounded border px-5 h-10 w-full"
           placeholder="Get weekly AI tools updates"
         />
         <button
