4f6f824c2dfe9014bff5cd2b5a1741e4e709fc90
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-19 12:45:49 +0530

    change footer hr color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <hr className="text-amber-50 opacity-50 mt-5" />
-      <p className="py-8 px-4 w-full text-center font-mono text-amber-50">
+      <hr className="border-violet-500 opacity-50 mt-5" />
+      <p className="py-8 px-4 w-full text-center font-poppins text-amber-50">
         © {currentYear} ai4devs · Made for developers who love to ship fast.
       </p>
     </footer>
