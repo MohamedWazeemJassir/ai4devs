13ca6f7c3fed84a3fd586eccb9ff1f95cc995aa1
wazeem <mohamedwazeemjassir@gmail.com>
2026-07-19 12:40:21 +0530

    add hr in mobile header and style logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,28 @@
 function Header() {
   return (
-    <header className="w-full flex py-12 font-mono text-center">
-      <a
-        href="/"
-        className="w-full sm:w-fit sm:ml-25 text-2xl font-medium tracking-tight hover:text-violet-500"
-      >
-        ai4devs
-      </a>
-
-      <form action="" className="hidden w-1/3 h-1/2 mr-15 sm:flex ml-auto">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          autoComplete="on"
-          className="border px-5 h-10 w-full"
-          placeholder="Your email address"  
-        />
-        <button
-          type="submit"
-          className="rounded font-semibold bg-violet-500 ml-3 p-2 hover:bg-violet-800"
-        >
-          Subscribe
-        </button>
-      </form>
+    <header>
+      <div className="w-full flex py-12 font-mono text-center">
+        <span className="w-full sm:w-fit sm:ml-25 text-3xl italic font-bold font-poppins tracking-tight">
+          <span className="text-violet-500">AI</span>4Devs
+        </span>
+        <form action="" className="hidden w-1/3 h-1/2 mr-15 sm:flex ml-auto">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="on"
+            className="border px-5 h-10 w-full"
+            placeholder="Your email address"
+          />
+          <button
+            type="submit"
+            className="rounded font-semibold bg-violet-500 ml-3 p-2 hover:bg-violet-800"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
+      <hr className="block sm:hidden border-violet-500 opacity-50" />
     </header>
   );
 }
