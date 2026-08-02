a48fee96c47f458b5cdbec6b323b8a54f178e5ab
wazeem <mohamedwazeemjassir@gmail.com>
2026-08-02 23:02:54 +0530

    fix card position

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import * as Icons from "@lobehub/icons";
 function Card({ logo, name, url, description, category, pricing }) {
   const Avatar = Icons[logo]?.Avatar;
   return (
-    <div className="border border-zinc-800 bg-zinc-900 shadow-gray-300 hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500 p-6 w-full h-auto sm:w-1/5 rounded delay-75 duration-150 transition hover:-translate-y-2 space-y-3">
+    <div className="border border-zinc-800 bg-zinc-900 shadow-gray-300 hover:border-violet-500 hover:shadow-lg hover:shadow-violet-500 p-6 w-full h-fit rounded delay-75 duration-150 transition hover:-translate-y-2 space-y-3"> 
       <div className="flex gap-3 items-center">
         {Avatar && <Avatar size={40} />}
         <h2 className="text-2xl">{name}</h2>
